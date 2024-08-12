078a3ebba647ae79fc63f582f154628fb2006a77
adnan <adnankhan@gmail.com>
2024-08-12 17:29:01 +0530

    --- Auto Git Commit ---

--- file_destructure/main.tf
+++ file_destructure/main.tf
@@ -1,8 +1,24 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.61.0"
+    }
+  }
 }
 
-resource "aws_instance" "name" {
-  ami = ""
-  
+provider "aws" {
+  # Configuration options
+  region = var.region
 }
+
+##### Try to create one ubuntu server
+resource "aws_instance" "this" {
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  key_name                = var.key_name
+
+  tags = var.tags
+
+}
+
