9da3ba268f00ae99ee1d0564e6d9d9a026eef555
adnan <adnankhan@gmail.com>
2024-08-14 19:59:59 +0530

    --- Auto Git Commit ---

--- ashwin/task1.tf
+++ ashwin/task1.tf
@@ -23,7 +23,7 @@ depends_on = [
   aws_key_pair.adnan
 
 ]
-
+}
 provider "aws_key_pair" "adnan" {
   key_name = "aws_ash"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOWr1hGHRIUp51Zk7p8x/WP71g+6RKN3npw6csZZ0G20"
