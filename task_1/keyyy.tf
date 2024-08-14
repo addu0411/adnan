d614f31fc7a701fa620e7079fa77a117b214e703
adnan <adnankhan@gmail.com>
2024-08-14 17:34:51 +0530

    --- Auto Git Commit ---

--- task_1/keyyy.tf
+++ task_1/keyyy.tf
@@ -31,5 +31,5 @@ depends_on = [
 }
 resource "aws_key_pair" "roy" {
   key_name   = "aws_vikas"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDtkSj2VOFB/PDJCZNSualWNxf0x+DDYe88D44se3M0"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDtkSj2VOFB/PDJCZNSualWNxf0x+DDYe88D44se3M0K"
 }
