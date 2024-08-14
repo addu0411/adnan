78d69accdcfa75d68359a2d72cbed000044a594e
adnan <adnankhan@gmail.com>
2024-08-14 20:09:55 +0530

    --- Auto Git Commit ---

--- ashwin/task1.tf
+++ ashwin/task1.tf
@@ -15,7 +15,7 @@ provider "aws" {
 resource "aws_instance" "this" {
     ami = "ami-0ad21ae1d0696ad58"
     instance_type = "t2.micro"
-    key_name = "aws_ash"
+    key_name = "aws_ashwin"
 
 
 depends_on = [
@@ -25,6 +25,6 @@ depends_on = [
 ]
 }
 resource "aws_key_pair" "adnan" {
-  key_name = "aws_ash"
+  key_name = "aws_ashwin"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOWr1hGHRIUp51Zk7p8x/WP71g+6RKN3npw6csZZ0G20"
 }
