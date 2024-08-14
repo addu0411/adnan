e8bb6ab56862052eb7360267bb9dd51d0d326151
adnan <adnankhan@gmail.com>
2024-08-14 19:57:51 +0530

    --- Auto Git Commit ---

--- ashwin/task1.tf
+++ ashwin/task1.tf
@@ -13,7 +13,14 @@ provider "aws" {
 }
 
 provider "aws_instance" {
-    ami = ""
+    ami = "#!/bin/bash
+wget https://releases.hashicorp.com/terraform/1.9.4/terraform_1.9.4_linux_amd64.zip
+sudo apt install zip -y
+unzip terraform_1.9.4_linux_amd64.zip
+cd /home/ubuntu/
+sudo mv terraform /usr/local/bin
+sudo rm -rfv terraform_1.9.4_linux_amd64.zip
+"
     instance_type = "t2.micro"
     key_name = "aws_ash"
 
