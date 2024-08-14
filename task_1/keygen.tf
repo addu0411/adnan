c78914e7fe3575780bbb41969e2d55ba085e7da7
adnan <adnankhan@gmail.com>
2024-08-14 11:22:44 +0530

    --- Auto Git Commit ---

--- task_1/keygen.tf
+++ task_1/keygen.tf
@@ -31,37 +31,6 @@ depends_on = [
 ]
 
 }
-
-
-resource "aws_security_group" "main" {
-  egress = [
-    {
-      cidr_blocks      = [ "0.0.0.0/0", ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
- ingress                = [
-   {
-     cidr_blocks      = [ "0.0.0.0/0", ]
-     description      = ""
-     from_port        = 22
-     ipv6_cidr_blocks = []
-     prefix_list_ids  = []
-     protocol         = "tcp"
-     security_groups  = []
-     self             = false
-     to_port          = 22
-  }
-  ]
-}
-
 resource "aws_key_pair" "devkey" {
   key_name   = "aws_key"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwd/NhSh+V5MZ5jLS7O1nNT8H7xqvcslLTUbY89DtAB"
