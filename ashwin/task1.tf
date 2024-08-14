5fca0cb03bfd5aa045afb7d7161ca03f55dfa6fe
adnan <adnankhan@gmail.com>
2024-08-14 20:06:01 +0530

    --- Auto Git Commit ---

--- ashwin/task1.tf
+++ ashwin/task1.tf
@@ -12,7 +12,7 @@ provider "aws" {
   
 }
 
-resource "aws_instance" {
+resource "aws_instance" "this" {
     ami = "ami-0ad21ae1d0696ad58"
     instance_type = "t2.micro"
     key_name = "aws_adnan"
