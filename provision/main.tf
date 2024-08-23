d6a3768e0e1756e30842c8f870d6d5aecd5a3b95
adnan <adnankhan@gmail.com>
2024-08-23 16:22:25 +0530

    --- Auto Git Commit ---

--- provision/main.tf
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://releases.hashicorp.com/terraform/1.9.4/terraform_1.9.4_linux_amd64.zip
-sudo apt install zip -y
-unzip terraform_1.9.4_linux_amd64.zip
-cd /home/ubuntu/
-sudo mv terraform /usr/local/bin
-sudo rm -rfv terraform_1.9.4_linux_amd64.zip
